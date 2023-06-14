d686edcaf3e6872a0052ac5ed19ea3c0a110e119
ramirochangomoreno <rama.exe.13@gmail.com>
2023-06-14 14:48:29 -0300

    configurando host de entradas

--- app.py
+++ app.py
@@ -1,10 +1,28 @@
 from fastapi import FastAPI
 from router.user import user
+from fastapi.middleware.cors import CORSMiddleware
 
 app=FastAPI()
 
-app.include_router(user)
 
+origins=[
+    'http://localhost:8000',
+    'http://localhost:3000',
+    'http://172.16.3.94:3000',
+    'http://172.16.3.94:8000',
+    'http://localhost:3000/dashboard'
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+app.include_router(user)
 @app.get('/')
 def helloword():
-    return "estamos con faztAPi"
+    return "Ramiro Quiroga DeveloperWeb | Sistema Unidosis Backend | FastAPI"
--- router/user.py
+++ router/user.py
@@ -4,21 +4,6 @@ from schemas.paciente import pacienteEntity,pacientesEntity
 
 user=APIRouter()
 
-# try:
-#     print('conexion exitosa')
-#     cursor = connection.cursor()
-#     cursor.execute("exec MedicamentosPorSala @Servicio='UTI'")
-#     rows=cursor.fetchall()
-#     for row in rows:
-#         row_to_list = [elem for elem in row]
-#         schema=pacienteEntity(row_to_list)
-#         print(schema)
-# except Exception as ex:
-#     print(ex)
-# finally:
-#     connection.close()
-#     print('coneccion finalizada')
-
 
 @user.get('/user/{servicio}')
 def get_user(servicio:str):
@@ -32,7 +17,7 @@ def get_user(servicio:str):
             row_to_list = [elem for elem in row]
             schema=pacienteEntity(row_to_list)
             myArray.append(schema)
-        return myArray
+        return {servicio:myArray}
     except Exception as ex:
         print(ex)
     # finally:
