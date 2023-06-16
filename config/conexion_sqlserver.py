f93a807b436a5a4b1ed2b8f53c7c250cbc80ab4f
RamiroQuiroga <89798864+RamiroQuiro@users.noreply.github.com>
2023-06-16 12:13:19 -0300

    Update conexion_sqlserver.py

--- config/conexion_sqlserver.py
+++ config/conexion_sqlserver.py
@@ -1,7 +1,7 @@
 import pyodbc
 from schemas.paciente import pacienteEntity,pacientesEntity
 
-connection=pyodbc.connect('DRIVER={SQL Server};SERVER=172.16.1.96;DATABASE=his;UID=sa;PWD=')
+connection=pyodbc.connect('DRIVER={SQL Server};SERVER={ip};DATABASE={name};UID=usiario;PWD=password')
 # try:
 #     # connection=pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER=172.16.3.94;DATABASE=registrobilletes;UID=ramiro;PWD=quirogon')
 #     print('conexion exitosa')
